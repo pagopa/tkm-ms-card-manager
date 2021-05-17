2badc1a7384f5b6f01759e9a48e624560bb3e45d
David Bertoni <David Bertoni>
2021-05-17 10:46:28 +0200

    Initializing project

---
+++ src/main/java/it/gov/pagopa/tkm/ms/consentmanager/CardManagerApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.tkm.ms.consentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagerApplication.class, args);
+	}
+
+}
