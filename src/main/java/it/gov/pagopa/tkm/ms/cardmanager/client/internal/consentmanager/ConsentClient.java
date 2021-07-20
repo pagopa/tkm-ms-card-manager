fbff722c2e3d8029a0bc3b24c7b4f13a7225e613
andrea rovere <e-aarv@gft.com>
2021-07-20 12:10:37 +0200

    PM-64 card manager circuit breaker

--- src/main/java/it/gov/pagopa/tkm/ms/cardmanager/client/internal/consentmanager/ConsentClient.java
+++ src/main/java/it/gov/pagopa/tkm/ms/cardmanager/client/internal/consentmanager/ConsentClient.java
@@ -3,17 +3,25 @@ package it.gov.pagopa.tkm.ms.cardmanager.client.internal.consentmanager;
 import it.gov.pagopa.tkm.ms.cardmanager.model.request.*;
 import org.springframework.cloud.openfeign.*;
 import org.springframework.web.bind.annotation.*;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 import static it.gov.pagopa.tkm.ms.cardmanager.constant.ApiParams.*;
 
 @FeignClient(value = "consent-card", url = "${client-urls.consent-manager}")
 public interface ConsentClient {
 
+    @CircuitBreaker(name = "consentClientCircuitBreaker", fallbackMethod = "geConsentFallback")
     @GetMapping("/consent")
     ConsentResponse getConsent(
             @RequestHeader(TAX_CODE_HEADER) String taxCode,
             @RequestParam(value = HPAN_QUERY_PARAM, required = false) String hpan,
             @RequestParam(value = SERVICES_QUERY_PARAM, required = false) String[] services
     );
+
+    public String getParFallback(String accountNumber, Throwable t ){
+        log.info(String.format("MASTERCARD fallback for get par of account number %s- cause {}", accountNumber), t.toString());
+        return "MASTERCARD fallback for get par. Some error occurred while calling get Par for Mastercard client";
+    }
+
 
 }
