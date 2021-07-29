e00664cb0ae5acd58d069acc73f08ab211474bde
andrea rovere <e-aarv@gft.com>
2021-07-29 12:34:01 +0200

    PM-64 optmize import

--- src/main/java/it/gov/pagopa/tkm/ms/cardmanager/service/CircuitBreakerManager.java
+++ src/main/java/it/gov/pagopa/tkm/ms/cardmanager/service/CircuitBreakerManager.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.tkm.ms.cardmanager.service;
 import it.gov.pagopa.tkm.ms.cardmanager.client.external.rtd.RtdHashingClient;
 import it.gov.pagopa.tkm.ms.cardmanager.client.internal.consentmanager.ConsentClient;
 import it.gov.pagopa.tkm.ms.cardmanager.model.entity.TkmCard;
-import it.gov.pagopa.tkm.ms.cardmanager.model.request.ConsentResponse;
 
 public interface CircuitBreakerManager {
 
