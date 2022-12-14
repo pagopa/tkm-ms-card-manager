9799699d253266140157f60e43d1bc8cddc291ac
David Bertoni <david.bertoni@gft.com>
2022-12-14 17:30:56 +0100

    PM-1242 - Adding transactional

--- src/main/java/it/gov/pagopa/tkm/ms/cardmanager/controller/CardDetailsController.java
+++ src/main/java/it/gov/pagopa/tkm/ms/cardmanager/controller/CardDetailsController.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.tkm.ms.cardmanager.controller;
 
 import it.gov.pagopa.tkm.ms.cardmanager.model.response.*;
 import org.springframework.http.*;
+import org.springframework.transaction.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import static it.gov.pagopa.tkm.ms.cardmanager.constant.ApiEndpoints.BASE_PATH_CARDS;
@@ -10,6 +11,7 @@ import static it.gov.pagopa.tkm.ms.cardmanager.constant.ApiParams.HPAN_HEADER;
 @RequestMapping(BASE_PATH_CARDS)
 public interface CardDetailsController {
 
+    @Transactional
     @GetMapping
     ResponseEntity<CardDetailsResponse> getCardDetails(@RequestHeader(name = HPAN_HEADER) String hpan);
 
